742605cd016ddf2091a982a8cda14a5812a129ce
Daniel <daniel.arulraj89@gmail.com>
2019-12-20 14:56:14 -0600

    Update test2_copy2 copy.js

--- test2_copy2 copy.js
+++ test2_copy2 copy.js
@@ -14,3 +14,46 @@ console.log("B");
 debugger;
 console.log("C");
 debugger; console.log("D");debugger;if (foo == null) {bar();}
+
+
+debugger;
+
+console.log("E");
+
+function footwo() {
+  var bar;
+  let baz;
+}
+
+let foo = function() {
+  try {
+    return 1;
+  } catch (err) {
+    return 2;
+  } finally {
+    return 3;
+  }
+};
+
+try {
+  doSomethingThatMightThrow();
+} catch (e) {
+  throw e;
+}
+
+try {
+  doSomethingThatMightThrow();
+} catch (e) {
+  throw e;
+} finally {
+  cleanUp();
+}
+
+var roo = function rar() {};
+roo = function rar() {};
+obj.roo = function rar() {};
+obj["roo"] = function rar() {};
+var obj = { roo: function rar() {} };
+({ ["roo"]: function rar() {} });
+
+let newVar = Array(0, 1, 2);
