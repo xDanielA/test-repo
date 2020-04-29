0065902ca531491d2434e47e65c9dc5dc105353f
Daniel <daniel.arulraj89@gmail.com>
2020-04-29 15:00:36 -0500

    new comit

--- sql.go
+++ sql.go
@@ -5,27 +5,6 @@ import (
 	"log"
 )
 
-func Status(username string) string {
-	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/hello")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer db.Close()
-	sql := "SELECT * FROM user WHERE username='" + username + "'"
-	row := db.QueryRow(sql)
-
-	var isLogged bool
-	row.Scan(&isLogged)
-	if isLogged {
-		return "online"
-	}
-
-	return "offline"
-}
-
-
-
 const getAssetType = () => {
 
     if (selType[0].type) { 
@@ -56,5 +35,36 @@ const getAssetType = () => {
     }
     return [""];
   };
+	
+
+func Status(username string) string {
+	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/hello")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+	sql := "SELECT * FROM user WHERE username='" + username + "'"
+	row := db.QueryRow(sql)
+
+	var isLogged bool
+	row.Scan(&isLogged)
+	if isLogged {
+		return "online"
+	}
+
+	return "offline"
+}
+
+ 
 
 
+
+
+	
+	
+
+
+
+
+	
